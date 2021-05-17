8d0fe76ffb7a1d565d2763cdef77b619963fc5c3
Ankit <ankitrastogi31200@gmail.com>
2021-05-17 07:49:53 +0530

    change1

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -16,7 +16,7 @@ const authLink = setContext(()=>{
 })
 
 const httpLink = createHttpLink({
-    uri:'http://localhost:5000'
+    uri:'https://peaceful-retreat-27995.herokuapp.com/'
 })
 
 const client = new ApolloClient({
